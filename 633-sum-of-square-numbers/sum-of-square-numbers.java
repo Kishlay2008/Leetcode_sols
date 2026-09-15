49dd8564a50af71f011253d09704b5e308e43766
kishlay raj <kishlayraj007@outlook.com>
2026-09-15 06:51:47 +0530

    Time: 4 ms (82.47%) | Memory: 42.3 MB (14.99%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,13 @@
+class Solution { 
+    public boolean judgeSquareSum(int c) {
+        long low = 0;
+        long high = (long) Math.sqrt(c);
+        while(low <= high){
+            long val = (low*low) + (high*high);
+            if(val==c) return true;
+            else if(val > c) high--;
+            else low++;
+        }
+        return false;
+    }
+}
